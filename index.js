ae5743eb7638b395b9cc0b5c3b65b85f5790e3b4
thencious <danielyin0920@hotmail.com>
2017-07-21 11:03:22 +0100

    added Procfile along with other things

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const expressLayouts = require('express-ejs-layouts');
 const { port, db }   = require('./config/env');
 const routes         = require('./config/routes');
 const app            = express();
+const mongoose       = require('mongoose');
+
+mongoose.Promise = require('bluebird');
+mongoose.connect(db);
 
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
